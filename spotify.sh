621dae5d7b691027bd756fe2a1e62e2d716ec7dc
derekagraham78 <54497759+derekagraham78@users.noreply.github.com>
2019-09-04 06:40:10 -0400

    Add files via upload
    fixed display errors and kill pid

--- spotify.sh
+++ spotify.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 IFS=$'\n\t'
 
-scroll=(unbuffer zscroll -l 10 -d 0.5)
+scroll=(unbuffer zscroll -l 10 -d 0.1)
 icon_playing=$'\u25B6'
 icon_paused=$'\u23F8'
 
-trap 'kill "$pid"' SIGINT SIGTERM
-
-set -x
+# trap '{ kill -"$pid"; exit 0;}' SIGINT SIGTERM
 
 while true; do
 	# requires playerctl>=2.0
@@ -16,12 +14,13 @@ while true; do
 	while read -r status line; do
 		kill "$pid" > /dev/null 2>&1
 		case $status in
-			Paused) "${scroll[@]}" -b ''"$icon_paused " "$line"'' & ;;
-			Playing)"${scroll[@]}" -b ''"$icon_playing " "$line"'' & ;;
+			Paused) "${scroll[@]}" -b "$icon_paused " "$line" & ;;
+			Playing) "${scroll[@]}" -b "$icon_playing " "$line" & ;;
 			*) echo "(STOPPED)" ;;
 		esac
+		trap '{ kill "$pid"; exit 0;}' SIGINT SIGTERM
 		pid=$!
-	done
+	done &
 	# no current players
 	echo '(STOPPED)'
 	sleep 15
