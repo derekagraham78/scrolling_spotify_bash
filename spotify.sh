ddc4bf9276f2b0f2cb14260449e74cf37469ae20
derekagraham78 <54497759+derekagraham78@users.noreply.github.com>
2019-09-04 08:08:02 -0400

    Add files via upload
    Fully working now
    Requires nerd-fonts

--- spotify.sh
+++ spotify.sh
@@ -5,22 +5,19 @@ scroll=(unbuffer zscroll -l 10 -d 0.1)
 icon_playing=$'\u25B6'
 icon_paused=$'\u23F8'
 
-# trap '{ kill -"$pid"; exit 0;}' SIGINT SIGTERM
-
 while true; do
 	# requires playerctl>=2.0
 	playerctl --follow metadata --format \
 		$'{{status}}\t{{artist}} - {{title}}' |
 	while read -r status line; do
-		kill "$pid" > /dev/null 2>&1
+		(( ${#pid} )) && kill "$pid" > /dev/null 2>&1
 		case $status in
 			Paused) "${scroll[@]}" -b "$icon_paused " "$line" & ;;
 			Playing) "${scroll[@]}" -b "$icon_playing " "$line" & ;;
 			*) echo "(STOPPED)" ;;
 		esac
-		trap '{ kill "$pid"; exit 0;}' SIGINT SIGTERM
 		pid=$!
-	done &
+	done
 	# no current players
 	echo '(STOPPED)'
 	sleep 15
